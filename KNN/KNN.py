7337c22855c91bc630bea91411c24a39b2497958
Gabriel Medina <gmedina197@gmail.com>
2018-08-16 00:58:33 -0300

    =D3

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -16,31 +16,8 @@ class KNN:
     def train(self):
         return
 
-    def predict(self):
-        candidates = []
-        for test_index, test_row in self.x_test.iterrows():
-            test = np.array(test_row)
-            distances = []
-            for train_index, train_row in self.x_train.iterrows():
-                d = np.linalg.norm(test - np.array(train_row))
-                distances.append(d)
-            distances = np.array(distances)
-
-            if self.distance_type == "sd":
-                distances = (distances - np.min(distances)) / (np.max(distances) - np.min(distances))
-            final_distance = []
-
-            for index, d in enumerate(distances):
-                if self.distance_type == "ied":
-                    final_distance.append((1 / d, index))
-
-                elif self.distance_type == "sd":
-                    final_distance.append((1 - d, index))
-
-            candidates.append(sorted(final_distance)[:self.k])
-
+    def compute_votes(self, candidates):
         predictions = []
-
         for votes in candidates:
             votes_count = {}
             for vote in votes:
@@ -56,11 +33,38 @@ class KNN:
                     elif self.vote_type == "pv":
                         votes_count[v] += 1 / vote[0]
             predictions.append(sorted(votes_count.items())[0])
+        return predictions
+
+    def predict(self):
+        candidates = []
+
+        for test_index, test_row in self.x_test.iterrows():
+            test = np.array(test_row)
+            distances = []
+            final_distance = []
+
+            for train_index, train_row in self.x_train.iterrows():
+                dist = np.linalg.norm(test - np.array(train_row))
+                distances.append(dist)
+
+            distances = np.array(distances)
+
+            for index, d in enumerate(distances):
+                if self.distance_type == "ied":
+                    final_distance.append((1 / d, index))
+
+                elif self.distance_type == "sd":
+                    distances = (distances - np.min(distances)) / (np.max(distances) - np.min(distances))
+                    final_distance.append((1 - d, index))
+
+            candidates.append(sorted(final_distance)[:self.k])
+
+        predictions = self.compute_votes(candidates)
 
         sum_predicions = 0
 
-        for index, p in enumerate(predictions):
-            expected = self.y_test.iloc[index][0]
-            sum_predicions += expected == p[0]
+        for idx, predicted in enumerate(predictions):
+            expected = self.y_test.iloc[idx][0]
+            sum_predicions += expected == predicted[0]
 
         return sum_predicions / len(self.x_test)
--- KNN/main.py
+++ KNN/main.py
@@ -6,31 +6,23 @@ import KNN
 from sklearn.model_selection import KFold
 
 
-def get_data():
-    df = pd.read_csv('Diabetes.csv')
-    return np.split(df, [len(df.columns) - 1], axis=1)
-
-
-def get_folds(dataset):
-    kf = KFold(n_splits=5, shuffle=True)
-    return kf.split(dataset)
-
-
 k_value = 3
-x, y = get_data()
-media_result = []
+df = pd.read_csv('Diabetes.csv')
+x, y = np.split(df, [len(df.columns) - 1], axis=1)
+media = []
 
 for j in range(0, 10):
-    kfolds = get_folds(x)
+    kf = KFold(n_splits=5, shuffle=True)
+    folds = kf.split(x)
     fold_result = []
-    for fold in kfolds:
+    for fold in folds:
         X_train, X_test = x.iloc[fold[0]], x.iloc[fold[1]]
         Y_train, Y_test = y.iloc[fold[0]], y.iloc[fold[1]]
-        classifier = KNN.KNN(X_train, Y_train, X_test, Y_test, k_value, distance_type="sd", vote_type="mv")
+        classifier = KNN.KNN(X_train, Y_train, X_test, Y_test, k_value, "sd", "mv")
         classifier.train()
         fold_result.append(classifier.predict())
     print("Execucao %s" % j)
     print(np.sum(fold_result) / 5)
-    media_result.append(np.sum(fold_result) / 5)
-print("Resultado medio: %s" % ((np.sum(media_result) / 10)*100))
-print(np.sum(media_result) / 10)
+    media.append(np.sum(fold_result) / 5)
+print("----")
+print("Resultado medio: %s" % ((np.sum(media) / 10)*100))
